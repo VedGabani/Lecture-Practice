bc6583be0e6dc2001fb512dbea77561cfb079a56
Ved Gabani <ved3443gaming@gmail.com>
2026-04-22 10:13:42 +0530

    Delete Section_3 directory

--- Section_3/Program_3.2.py
+++
@@ -1,9 +0,0 @@
-# Uppercase and Lowercase
-
-print("----- Uppercase and Lowercase -----\n")
-
-s = input("Enter a string -_- ")
-print("\n")
-
-print("Uppercase -_- ", s.upper())
-print("Lowercase -_- ", s.lower())
