ca69dacde67dd8cbd2a0917fb20c691320ef3412
Ved Gabani <ved3443gaming@gmail.com>
2026-04-26 09:22:43 +0530

    Add files via upload

---
+++ 25-04-2026/Person.py
@@ -0,0 +1,18 @@
+# Chat Program (Loop)
+
+print("----- Chat Program (Loop) -----\n")
+
+p1 = input("Enter Person 1 name -_- ")
+print("\n")
+
+p2 = input("Enter Person 2 name -_- ")
+print("\n")
+
+for i in range(3):
+    msg1 = input(p1 + " -_- ")
+    print("\n")
+
+    msg2 = input(p2 + " -_- ")
+    print("\n")
+
+print("Conversation ended -_-")
