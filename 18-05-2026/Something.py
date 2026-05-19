7954051bd1082a85b722ae873f08a5fefb6714e3
Ved Gabani <ved3443gaming@gmail.com>
2026-05-19 10:37:01 +0530

    Add files via upload

---
+++ 18-05-2026/Something.py
@@ -0,0 +1,85 @@
+'''
+
+OOP (object orinted program)
+mainly has 4 pillars
+
+1. Encapsulation
+2. Inheritance
+3. Polymerphism
+4. Abstraction
+
+Python object classes
+
+1. Encapsulation
+
+warping data (varabile) & method (function)
+together inside a single class
+
+It also helps in data hiding & security
+
+Real time example
+
+Think about ATM machine
+you can use ATM function but internal banking
+detail are hidden
+
+Advantage
+
+1) Data security
+2) Prevent direct access
+3) Better control over data
+
+2. Inheritance
+
+one class can use properties & method of another
+calss
+
+parent calss features can be reusd in child calss
+
+Rel time example
+
+
+Father --> Son
+
+Surname
+Property
+Features
+
+Advantage
+
+1) Code reusability
+2) Less code duplication
+3) Easy maintance
+4) Type of inheritance
+5) Type of meaning
+
+3. Polymerphism
+
+Same method name behave diffrently
+
+Real time example
+
+Father at home
+Boss(father) at office
+Friend at outside
+
+Advantage
+
+1) Flexibility
+2) Reusable method
+3) Cleaner code
+4) Different argument
+
+4. Abstraction
+
+Hiding internal inplematation & Showing
+only importance feature
+
+Advantage
+
+1) Hides complexity
+2) Better security
+3) Cleaner code
+4) Code abstraction
+
+'''
